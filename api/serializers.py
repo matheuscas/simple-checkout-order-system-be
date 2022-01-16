f5b186d5b2f93a63a32446a003660be117194990
matheuscas <matheus.mcas@gmail.com>
2022-01-16 15:43:54 -0300

    Create PaymentSerializer and update tests

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 from rest_framework import serializers
 from rest_framework.exceptions import ParseError
 
-from pos.models import Item, Order
+from pos.models import Item, Order, Payment
 
 EMPTY_ITEMS_ERROR_MESSAGE = "Items cannot be empty"
 ZERO_TOTAL_ERROR_MESSAGE = "Total cannot be zero"
@@ -12,17 +12,24 @@ ZERO_TOTAL_ERROR_MESSAGE = "Total cannot be zero"
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ["id", "category", "image_id", "name", "price", "quantity"]
+        fields = ["id", "category", "image_id", "name", "price"]
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = "__all__"
 
 
 class OrderSerializer(serializers.ModelSerializer):
     items = serializers.PrimaryKeyRelatedField(
         many=True, queryset=Item.objects.all()
     )
+    payment = PaymentSerializer()
 
     class Meta:
         model = Order
-        fields = ["total", "items"]
+        fields = ["total", "items", "payment"]
 
     def create(self, validated_data):
         items = validated_data.pop("items")
@@ -31,6 +38,7 @@ class OrderSerializer(serializers.ModelSerializer):
         total = Decimal(validated_data.get("total", "0"))
         if total.is_zero():
             raise ParseError(ZERO_TOTAL_ERROR_MESSAGE)
-        order = Order.objects.create(**validated_data)
+        payment = Payment.objects.create(**validated_data.pop("payment"))
+        order = Order.objects.create(total=total, payment=payment)
         order.items.add(*items)
         return order
--- api/tests.py
+++ api/tests.py
@@ -5,7 +5,7 @@ import pytest
 from model_bakery import baker
 
 from api.serializers import EMPTY_ITEMS_ERROR_MESSAGE, ZERO_TOTAL_ERROR_MESSAGE
-from pos.models import Item
+from pos.models import Item, Payment
 
 ITEMS_QUANTITY_FIXTURE = random.randint(1, 10)
 MENU_ENDPOINT = "/api/v1/menu"
@@ -24,6 +24,19 @@ def items():
     return baker.make(Item, _quantity=ITEMS_QUANTITY_FIXTURE)
 
 
+@pytest.fixture
+def payment_dict():
+    payment = baker.prepare(Payment, address="Some address")
+    return {
+        "payment": {
+            "name": payment.name,
+            "email": payment.email,
+            "address": payment.address,
+            "payment_mode": payment.payment_mode,
+        }
+    }
+
+
 @pytest.mark.django_db
 def test_get_menu_items(items, api_client):
     response = api_client.get(MENU_ENDPOINT)
@@ -32,23 +45,31 @@ def test_get_menu_items(items, api_client):
 
 
 @pytest.mark.django_db
-def test_submit_order(items, api_client):
-    payload = {"items": [item.id for item in items], "total": "91.45"}
+def test_submit_order(items, payment_dict, api_client):
+    payload = {
+        "items": [item.id for item in items],
+        "total": "91.45",
+        **payment_dict,
+    }
     response = api_client.post(ORDER_ENDPOINT, payload, format="json")
     assert response.status_code == HTTPStatus.CREATED
 
 
 @pytest.mark.django_db
-def test_submit_order_empty_items(items, api_client):
-    payload = {"items": [], "total": "91.45"}
+def test_submit_order_empty_items(items, payment_dict, api_client):
+    payload = {"items": [], "total": "91.45", **payment_dict}
     response = api_client.post(ORDER_ENDPOINT, payload, format="json")
     assert response.status_code == HTTPStatus.BAD_REQUEST
     assert {"detail": EMPTY_ITEMS_ERROR_MESSAGE} == response.json()
 
 
 @pytest.mark.django_db
-def test_submit_order_total_zero(items, api_client):
-    payload = {"items": [item.id for item in items], "total": "0.00"}
+def test_submit_order_total_zero(items, payment_dict, api_client):
+    payload = {
+        "items": [item.id for item in items],
+        "total": "0.00",
+        **payment_dict,
+    }
     response = api_client.post(ORDER_ENDPOINT, payload, format="json")
     assert response.status_code == HTTPStatus.BAD_REQUEST
     assert {"detail": ZERO_TOTAL_ERROR_MESSAGE} == response.json()
